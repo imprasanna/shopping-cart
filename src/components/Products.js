f89b0a4905c028c234c622a773c1ef61970de208
Prasanna <prasannaacharya2073@gmail.com>
2023-03-22 15:21:25 +0545

    Now, all items route into their individual product page on click

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,8 +5,12 @@ import Grid from "@mui/material/Grid";
 import { products } from "../utilities/constants";
 import ProductDetails from "./ProductDetails";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { showProductDetails } from "../store/slices/CartSlice";
 
 const Products = () => {
+  const dispatch = useDispatch();
+
   const [hoverItemId, setHoveredItemId] = useState(null);
 
   const elevateBox = (productId) => {
@@ -17,18 +21,26 @@ const Products = () => {
     setHoveredItemId(null);
   };
 
+  const handleProductOpen = (id) => {
+    const foundProduct = products.find((product) => product.id === id);
+    console.log(foundProduct);
+    dispatch(showProductDetails(foundProduct));
+  };
+
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           {products.slice(0, 8).map((product) => {
             return (
-              <Grid xs={3} item key={product.id}>
+              <Grid xs={3} item>
                 <Link
                   to={`/products/${product.id}`}
                   style={{ textDecoration: "none" }}
                 >
                   <Paper
+                    key={product.id}
+                    onClick={() => handleProductOpen(product.id)}
                     sx={{ padding: "10px", minHeight: "400px" }}
                     elevation={hoverItemId === product.id ? 6 : 0}
                     onMouseEnter={() => elevateBox(product.id)}
