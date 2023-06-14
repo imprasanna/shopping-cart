1f8838f769b78e0ce019155140d2aa58783c8ed5
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-06-14 12:17:00 +0545

    Cors middleware activated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 require("dotenv").config();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: false }));
 // // parse application/json
 // app.use(bodyParser.json());
