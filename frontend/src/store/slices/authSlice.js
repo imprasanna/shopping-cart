ca06731333b5d49ef293920692fe86b7829f312e
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-06-14 14:23:16 +0545

    Redux thunk configured

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { url } from "./api";
+import jwtDecode from "jwt-decode"
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -12,13 +15,62 @@ const initialState = {
   userLoaded: false,
 };
 
+// Action creator using redux thunk
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (values, { rejectWithValue }) => {
+    try {
+      const token = await axios.post(`${url}/register`, {
+        name: values.name,
+        email: values.email,
+        password: values.password,
+      });
+
+      localStorage.setItem("token", token.data);
+
+      return token.data;
+
+    } catch(err) {
+        console.log(err);
+        return rejectWithValue(err.response.data);
+    }
+  }
+)
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     authReducer: () => {},
   },
-  extraReducers: {},
-});
+  // builder -> and object provided by redux toolkit
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.pending, (state, action) => {
+      return { ...state, registerStatus: "pending" }
+    });
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      if (action.payload) {
+
+        const user = jwtDecode(action.payload)
+
+        return {
+          ...state,
+          token: action.payload,
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+          registerStatus: "success",
+        }
+      }
+    });
+    builder.addCase(registerUser.rejected, (state, action) => {
+      return {
+        ...state,
+        registerStatus: "rejected",
+        registerError: action.payload,
+      }
+    });  
+  },
+  });
 
 export default authSlice.reducer;
