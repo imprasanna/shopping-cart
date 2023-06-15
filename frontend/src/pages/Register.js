bc9d28dc04fa4e97e7a52fc2ec830ab34dd144d5
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-06-15 20:07:08 +0545

    Sign up process managed properly, redux thunk removed

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,38 +1,67 @@
 import { useState } from "react";
 import logo from "../Assets/logo.png";
-import { useDispatch, useSelector } from "react-redux";
-import { registerUser } from "../store/slices/authSlice";
-
+import { useDispatch } from "react-redux";
+// import { registerUser } from "../store/slices/authSlice";
+import { useNavigate } from "react-router-dom";
 import React from "react";
+import axios from "axios";
+import { login } from "../store/slices/authSlice";
 
 const Register = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const auth = useSelector((state) => state.auth);
+  // const auth = useSelector((state) => state.auth);
 
-  console.log(auth);
+  // console.log(auth);
 
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const setName = (ev) => {
-    setUser({ ...user, name: ev.target.value });
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   dispatch(registerUser(user));
+  //   navigate("/");
+  // };
+
+  const handleNameChange = (ev) => {
+    setName(ev.target.value);
   };
 
-  const setEmail = (ev) => {
-    setUser({ ...user, email: ev.target.value });
+  const handleEmailChange = (ev) => {
+    setEmail(ev.target.value);
   };
 
-  const setPassword = (ev) => {
-    setUser({ ...user, password: ev.target.value });
+  const handlePasswordChange = (ev) => {
+    setPassword(ev.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(registerUser(user));
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}/register`,
+        {
+          name,
+          email,
+          password,
+        }
+      );
+      // console.log("LOGIN SIGNUP RESPONSE", data);
+      // console.log("JSON stringify", JSON.stringify(data));
+      // save in local storage
+      localStorage.setItem("user", JSON.stringify(data));
+      // navigate to homepage after successful login through signup
+      navigate("/");
+      // dispatch the login data to the redux store
+      dispatch(login(data));
+      // toast.success("Signed Up Successfully!!");
+    } catch (err) {
+      // toast.error(err.response.data);
+      alert(err.response.data);
+    }
   };
 
   return (
@@ -43,22 +72,27 @@ const Register = () => {
         <input
           type="text"
           placeholder="Enter your name..."
-          onChange={setName}
+          value={name}
+          onChange={handleNameChange}
         />
         <br />
         <input
           type="email"
           placeholder="Enter your email..."
-          onChange={setEmail}
+          value={email}
+          onChange={handleEmailChange}
         />
         <br />
         <input
           type="password"
           placeholder="Enter your password..."
-          onChange={setPassword}
+          value={password}
+          onChange={handlePasswordChange}
         />
         <br />
-        <button onSubmit={handleSubmit}>Sign Up</button>
+        <button onClick={handleSubmit} disabled={!name || !email || !password}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,77 +1,100 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-import { url } from "./api";
-import jwtDecode from "jwt-decode";
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+// import { url } from "./api";
+// import jwtDecode from "jwt-decode";
+
+// const initialState = {
+//   token: localStorage.getItem("token"),
+//   name: "",
+//   email: "",
+//   _id: "",
+//   registerStatus: "",
+//   registerError: "",
+//   loginStatus: "",
+//   loginError: "",
+//   userLoaded: false,
+// };
+
+// // Action creator using redux thunk
+// export const registerUser = createAsyncThunk(
+//   "auth/registerUser",
+//   async (user, { rejectWithValue }) => {
+//     // user is the value
+//     try {
+//       const token = await axios.post(`${url}/register`, {
+//         name: user.name,
+//         email: user.email,
+//         password: user.password,
+//       });
+
+//       localStorage.setItem("token", token.data);
+
+//       console.log(token.data);
+
+//       return token.data;
+//     } catch (err) {
+//       console.log(err);
+//       return rejectWithValue(err.response.data);
+//     }
+//   }
+// );
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState,
+//   reducers: {
+//     authReducer: () => {},
+//   },
+//   // builder -> and object provided by redux toolkit
+//   extraReducers: (builder) => {
+//     builder.addCase(registerUser.pending, (state, action) => {
+//       return { ...state, registerStatus: "pending" };
+//     });
+//     builder.addCase(registerUser.fulfilled, (state, action) => {
+//       if (action.payload) {
+//         const user = jwtDecode(action.payload);
+
+//         return {
+//           ...state,
+//           token: action.payload,
+//           name: user.name,
+//           email: user.email,
+//           _id: user._id,
+//           registerStatus: "success",
+//         };
+//       }
+//     });
+//     builder.addCase(registerUser.rejected, (state, action) => {
+//       return {
+//         ...state,
+//         registerStatus: "rejected",
+//         registerError: action.payload,
+//       };
+//     });
+//   },
+// });
+
+// export default authSlice.reducer;
+
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  token: localStorage.getItem("token"),
-  name: "",
-  email: "",
-  _id: "",
-  registerStatus: "",
-  registerError: "",
-  loginStatus: "",
-  loginError: "",
-  userLoaded: false,
+  user: null,
 };
 
-// Action creator using redux thunk
-export const registerUser = createAsyncThunk(
-  "auth/registerUser",
-  async (user, { rejectWithValue }) => {
-    // user is the value
-    try {
-      const token = await axios.post(`${url}/register`, {
-        name: user.name,
-        email: user.email,
-        password: user.password,
-      });
-
-      localStorage.setItem("token", token.data);
-
-      console.log(token.data);
-
-      return token.data;
-    } catch (err) {
-      console.log(err);
-      return rejectWithValue(err.response.data);
-    }
-  }
-);
-
-const authSlice = createSlice({
+export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    authReducer: () => {},
-  },
-  // builder -> and object provided by redux toolkit
-  extraReducers: (builder) => {
-    builder.addCase(registerUser.pending, (state, action) => {
-      return { ...state, registerStatus: "pending" };
-    });
-    builder.addCase(registerUser.fulfilled, (state, action) => {
-      if (action.payload) {
-        const user = jwtDecode(action.payload);
-
-        return {
-          ...state,
-          token: action.payload,
-          name: user.name,
-          email: user.email,
-          _id: user._id,
-          registerStatus: "success",
-        };
-      }
-    });
-    builder.addCase(registerUser.rejected, (state, action) => {
-      return {
-        ...state,
-        registerStatus: "rejected",
-        registerError: action.payload,
-      };
-    });
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
   },
 });
+
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
